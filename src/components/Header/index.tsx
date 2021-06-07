caace0c8e78c9e7835006aa6430b31e9cd590fc3
José <josehenriqueiel@gmail.com>
2021-06-07 15:12:18 -0300

    Criação menu

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.scss';
+
+export function Header(){
+    return(
+        <header className={styles.HeaderContainer}>
+            <div className={styles.HeaderContent}>
+                <img src="/images/logo.svg" alt="ig.news" />
+
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Posts</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
